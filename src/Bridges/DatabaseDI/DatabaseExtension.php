73c24cf027e7b0b32116ee0e14c1f5634bfd0f6e
David Grudl <david@grudl.com>
2014-04-19 19:37:58 +0200

    typos in namespace

--- src/Bridges/DatabaseDI/DatabaseExtension.php
+++ src/Bridges/DatabaseDI/DatabaseExtension.php
@@ -69,7 +69,7 @@ class DatabaseExtension extends Nette\DI\CompilerExtension
 			$connection = $container->addDefinition($prefix . $this->prefix($name))
 				->setClass('Nette\Database\Connection', array($info['dsn'], $info['user'], $info['password'], $info['options']))
 				->setAutowired($info['autowired'])
-				->addSetup('Nette\Diagnostics\Debugger::getBlueScreen()->addPanel(?)', array(
+				->addSetup('Tracy\Debugger::getBlueScreen()->addPanel(?)', array(
 					'Nette\Bridges\DatabaseTracy\ConnectionPanel::renderException'
 				));
 

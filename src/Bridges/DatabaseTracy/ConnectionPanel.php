4b4a269d2b92d8fc922230996889128948a5e346
David Grudl <david@grudl.com>
2014-10-27 02:00:50 +0100

    Revert "Wrong queries in ConnectionPanel #31"
    This reverts commit d7ab57f00d3cb4d9f752ce4d17535250d0378e1c.
    
    - it quotes all parameters, not only strings
    - it breaks Helpers::dumpSql()

--- src/Bridges/DatabaseTracy/ConnectionPanel.php
+++ src/Bridges/DatabaseTracy/ConnectionPanel.php
@@ -70,10 +70,7 @@ class ConnectionPanel extends Nette\Object implements Tracy\IBarPanel
 		if ($result instanceof Nette\Database\ResultSet) {
 			$this->totalTime += $result->getTime();
 			if ($this->count < $this->maxQueries) {
-				$parameters = array_map(function ($param) use ($connection) {
-					return $connection->quote($param);
-				}, $result->getParameters());
-				$this->queries[] = array($connection, $result->getQueryString(), $parameters, $source, $result->getTime(), $result->getRowCount(), NULL);
+				$this->queries[] = array($connection, $result->getQueryString(), $result->getParameters(), $source, $result->getTime(), $result->getRowCount(), NULL);
 			}
 
 		} elseif ($result instanceof \PDOException && $this->count < $this->maxQueries) {

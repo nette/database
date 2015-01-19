36a97c58a6e35df91a1490c205c8d34575768868
David Grudl <david@grudl.com>
2015-01-19 18:26:14 +0100

    DatabaseExtension: updated for 2.3, added aliases

--- src/Bridges/DatabaseDI/DatabaseExtension.php
+++ src/Bridges/DatabaseDI/DatabaseExtension.php
@@ -30,37 +30,37 @@ class DatabaseExtension extends Nette\DI\CompilerExtension
 		'autowired' => NULL,
 	);
 
+	/** @var bool */
+	private $debugMode;
+
+
+	public function __construct($debugMode = FALSE)
+	{
+		$this->debugMode = $debugMode;
+	}
+
 
 	public function loadConfiguration()
 	{
-		$configs = $this->compiler->getConfig();
-		if (isset($configs['nette']['database'])) { // back compatibility
-			$configs = $configs['nette']['database'];
-			$prefix = 'nette.';
-		} else {
-			$configs = isset($configs[$this->name]) ? $configs[$this->name] : array();
-			$prefix = '';
-		}
-
+		$configs = $this->getConfig();
 		if (isset($configs['dsn'])) {
 			$configs = array('default' => $configs);
 		}
 
-		$autowired = TRUE;
+		$defaults = $this->databaseDefaults;
+		$defaults['autowired'] = TRUE;
 		foreach ((array) $configs as $name => $config) {
 			if (!is_array($config)) {
 				continue;
 			}
-			$this->validate($config, $this->databaseDefaults, 'database');
-
-			$config += array('autowired' => $autowired) + $this->databaseDefaults;
-			$autowired = FALSE;
-			$this->setupDatabase($config, $name, $prefix);
+			$config = $this->validateConfig($defaults, $config, $this->prefix($name));
+			$defaults['autowired'] = FALSE;
+			$this->setupDatabase($config, $name);
 		}
 	}
 
 
-	private function setupDatabase($config, $name, $prefix)
+	private function setupDatabase($config, $name)
 	{
 		$container = $this->getContainerBuilder();
 
@@ -71,15 +71,11 @@ class DatabaseExtension extends Nette\DI\CompilerExtension
 			}
 		}
 
-		$hasBlueScreenService = $container->hasDefinition('nette.blueScreen');
-		$connection = $container->addDefinition($prefix . $this->prefix($name))
+		$connection = $container->addDefinition($this->prefix($name))
 			->setClass('Nette\Database\Connection', array($config['dsn'], $config['user'], $config['password'], $config['options']))
-			->setAutowired($config['autowired'])
-			->addSetup($hasBlueScreenService ? '@nette.blueScreen::addPanel' : 'Tracy\Debugger::getBlueScreen()->addPanel(?)', array(
-				'Nette\Bridges\DatabaseTracy\ConnectionPanel::renderException'
-			));
+			->setAutowired($config['autowired']);
 
-		$structure = $container->addDefinition($prefix . $this->prefix("$name.structure"))
+		$structure = $container->addDefinition($this->prefix("$name.structure"))
 			->setClass('Nette\Database\Structure')
 			->setArguments(array($connection));
 
@@ -97,7 +93,7 @@ class DatabaseExtension extends Nette\DI\CompilerExtension
 			$conventions = NULL;
 
 		} elseif (is_string($config['conventions'])) {
-			$conventions = $container->addDefinition($prefix . $this->prefix("$name.$conventionsServiceName"))
+			$conventions = $container->addDefinition($this->prefix("$name.$conventionsServiceName"))
 				->setClass(preg_match('#^[a-z]+\z#', $config['conventions'])
 					? 'Nette\Database\Conventions\\' . ucfirst($config['conventions']) . 'Conventions'
 					: $config['conventions'])
@@ -109,21 +105,22 @@ class DatabaseExtension extends Nette\DI\CompilerExtension
 			$conventions = reset($tmp);
 		}
 
-		$container->addDefinition($prefix . $this->prefix("$name.context"))
+		$container->addDefinition($this->prefix("$name.context"))
 			->setClass('Nette\Database\Context', array($connection, $structure, $conventions))
 			->setAutowired($config['autowired']);
 
-		if ($container->parameters['debugMode'] && $config['debugger']) {
-			$connection->addSetup('Nette\Database\Helpers::createDebugPanel', array($connection, !empty($config['explain']), $name));
+		if ($config['debugger']) {
+			$connection->addSetup('@Tracy\BlueScreen::addPanel', array(
+				'Nette\Bridges\DatabaseTracy\ConnectionPanel::renderException'
+			));
+			if ($this->debugMode) {
+				$connection->addSetup('Nette\Database\Helpers::createDebugPanel', array($connection, !empty($config['explain']), $name));
+			}
 		}
-	}
 
-
-	private function validate(array $config, array $expected, $name)
-	{
-		if ($extra = array_diff_key($config, $expected)) {
-			$extra = implode(", $name.", array_keys($extra));
-			throw new Nette\InvalidStateException("Unknown option $name.$extra.");
+		if ($this->name === 'database') {
+			$container->addAlias("nette.database.$name", $this->prefix($name));
+			$container->addAlias("nette.database.$name.context", $this->prefix("$name.context"));
 		}
 	}
 

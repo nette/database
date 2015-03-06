f81fcb370209447cc85a2d4bf77c359ca853b9c0
Jakub Červenka <redwormik@gmail.com>
2015-03-06 13:26:06 +0100

    Table: fixed Selection::getReferencedTable() always refetching when primary is NULL

--- src/Database/Table/Selection.php
+++ src/Database/Table/Selection.php
@@ -809,7 +809,7 @@ class Selection extends Nette\Object implements \Iterator, IRowContainer, \Array
 		$referenced = & $this->refCache['referenced'][$this->getSpecificCacheKey()]["$table.$column"];
 		$selection = & $referenced['selection'];
 		$cacheKeys = & $referenced['cacheKeys'];
-		if ($selection === NULL || !isset($cacheKeys[$checkPrimaryKey])) {
+		if ($selection === NULL || ($checkPrimaryKey !== NULL && !isset($cacheKeys[$checkPrimaryKey]))) {
 			$this->execute();
 			$cacheKeys = array();
 			foreach ($this->rows as $row) {

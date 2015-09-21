d9b80b94f87cbf29d15e46c3866094ce3a354389
David Grudl <david@grudl.com>
2015-09-21 18:04:18 +0200

    SqlBuilder: applyLimit() is called always

--- src/Database/Table/SqlBuilder.php
+++ src/Database/Table/SqlBuilder.php
@@ -155,9 +155,7 @@ class SqlBuilder extends Nette\Object
 		$queryJoins = $this->buildQueryJoins($joins);
 		$query = "{$querySelect} FROM {$this->delimitedTable}{$queryJoins}{$queryCondition}{$queryEnd}";
 
-		if ($this->limit !== NULL || $this->offset) {
-			$this->driver->applyLimit($query, $this->limit, $this->offset);
-		}
+		$this->driver->applyLimit($query, $this->limit, $this->offset);
 
 		return $this->tryDelimite($query);
 	}

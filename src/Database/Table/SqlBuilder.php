5e379cdb0c5b66409c3550376f9acba68a9a641a
David Grudl <david@grudl.com>
2015-09-21 18:01:50 +0200

    SqlBuilder: applyLimit() is called for positive offset only to maintain compatibility

--- src/Database/Table/SqlBuilder.php
+++ src/Database/Table/SqlBuilder.php
@@ -155,8 +155,8 @@ class SqlBuilder extends Nette\Object
 		$queryJoins = $this->buildQueryJoins($joins);
 		$query = "{$querySelect} FROM {$this->delimitedTable}{$queryJoins}{$queryCondition}{$queryEnd}";
 
-		if ($this->limit !== NULL || $this->offset) {
-			$this->driver->applyLimit($query, $this->limit, $this->offset);
+		if ($this->limit !== NULL || $this->offset > 0) {
+			$this->driver->applyLimit($query, $this->limit, max(0, $this->offset));
 		}
 
 		return $this->tryDelimite($query);

99fe59ef73da8a20e14e34b21e47cb9e79762d55
Karel Hák <karel.hak@fregis.cz>
2016-02-25 22:39:31 +0100

    SqlBuilder: Added alias support

--- src/Database/Table/Selection.php
+++ src/Database/Table/Selection.php
@@ -406,6 +406,20 @@ class Selection extends Nette\Object implements \Iterator, IRowContainer, \Array
 	}
 
 
+	/**
+	 * Alias table.
+	 * @example ':book:book_tag.tag', 'tg'
+	 * @param string
+	 * @param string
+	 * @return self
+	 */
+	public function alias($tableChain, $alias)
+	{
+		$this->sqlBuilder->addAlias($tableChain, $alias);
+		return $this;
+	}
+
+
 	/********************* aggregations ****************d*g**/
 
 

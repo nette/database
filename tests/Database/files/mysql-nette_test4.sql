d32922e3c8b1c0b0b98167feb542283a6f5b300d
Samuel Kelemen <kelemen@efabrica.sk>
2017-01-26 12:37:46 +0100

    Mysql 5.7 tests fix

--- tests/Database/files/mysql-nette_test4.sql
+++ tests/Database/files/mysql-nette_test4.sql
@@ -1,5 +1,3 @@
-/*!40102 SET storage_engine = InnoDB */;
-
 DROP DATABASE IF EXISTS nette_test;
 CREATE DATABASE nette_test;
 USE nette_test;
@@ -8,28 +6,28 @@ CREATE TABLE simple_pk_autoincrement (
 	identifier1 int NOT NULL AUTO_INCREMENT,
 	note varchar(100),
 	PRIMARY KEY (identifier1)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE simple_pk_no_autoincrement (
 	identifier1 int NOT NULL,
 	note varchar(100),
 	PRIMARY KEY (identifier1)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE multi_pk_no_autoincrement (
 	identifier1 int NOT NULL,
 	identifier2 int NOT NULL,
 	note varchar(100),
 	PRIMARY KEY (identifier1, identifier2)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE multi_pk_autoincrement(
 	identifier1 int NOT NULL AUTO_INCREMENT,
 	identifier2 int NOT NULL,
 	note varchar(100),
 	PRIMARY KEY (identifier1, identifier2)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE no_pk (
 	note varchar(100)
-);
+) ENGINE=InnoDB;

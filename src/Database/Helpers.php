2c000fed3b89e0f3ed4ec7d552c5d7c107b618ec
David Grudl <david@grudl.com>
2017-05-24 14:48:45 +0200

    Helpers::loadFromFile added $onProgress

--- src/Database/Helpers.php
+++ src/Database/Helpers.php
@@ -177,9 +177,10 @@ class Helpers
 
 	/**
 	 * Import SQL dump from file - extremely fast.
+	 * @param  $onProgress  function (int $count, ?float $percent): void
 	 * @return int  count of commands
 	 */
-	public static function loadFromFile(Connection $connection, $file): int
+	public static function loadFromFile(Connection $connection, string $file, callable $onProgress = NULL): int
 	{
 		@set_time_limit(0); // @ function may be disabled
 
@@ -188,11 +189,13 @@ class Helpers
 			throw new Nette\FileNotFoundException("Cannot open file '$file'.");
 		}
 
-		$count = 0;
+		$stat = fstat($handle);
+		$count = $size = 0;
 		$delimiter = ';';
 		$sql = '';
 		$pdo = $connection->getPdo(); // native query without logging
 		while (($s = fgets($handle)) !== FALSE) {
+			$size += strlen($s);
 			if (!strncasecmp($s, 'DELIMITER ', 10)) {
 				$delimiter = trim(substr($s, 10));
 
@@ -201,6 +204,9 @@ class Helpers
 				$pdo->exec($sql);
 				$sql = '';
 				$count++;
+				if ($onProgress) {
+					$onProgress($count, isset($stat['size']) ? $size * 100 / $stat['size'] : NULL);
+				}
 
 			} else {
 				$sql .= $s;
@@ -209,6 +215,9 @@ class Helpers
 		if (rtrim($sql) !== '') {
 			$pdo->exec($sql);
 			$count++;
+			if ($onProgress) {
+				$onProgress($count, isset($stat['size']) ? 100 : NULL);
+			}
 		}
 		fclose($handle);
 		return $count;

544c1644a416a6eae40ec33b23260295bf4eaaa8
Ondřej Kubíček <kubon@kubon.cz>
2017-12-13 07:02:10 +0100

    fixed PHP 7.2 compatibility (#190)
    * fixed Warning: count(): Parameter must be an array or an object that implements Countable

--- src/Database/Table/GroupedSelection.php
+++ src/Database/Table/GroupedSelection.php
@@ -158,7 +158,7 @@ class GroupedSelection extends Selection
 			foreach ((array) $this->rows as $key => $row) {
 				$ref = &$data[$row[$this->column]];
 				$skip = &$offset[$row[$this->column]];
-				if ($limit === null || $rows <= 1 || (count($ref) < $limit && $skip >= $this->sqlBuilder->getOffset())) {
+				if ($limit === null || $rows <= 1 || (count($ref ?? []) < $limit && $skip >= $this->sqlBuilder->getOffset())) {
 					$ref[$key] = $row;
 				} else {
 					unset($this->rows[$key]);

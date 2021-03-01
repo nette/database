356c13b31d2630ff53a988d2bdc2c618e61a46e2
David Grudl <david@grudl.com>
2021-03-01 19:52:46 +0100

    added PHP 8 typehints

--- src/Database/ResultSet.php
+++ src/Database/ResultSet.php
@@ -218,9 +218,8 @@ class ResultSet implements \Iterator, IRowContainer
 
 	/**
 	 * Fetches single field.
-	 * @return mixed
 	 */
-	public function fetchField($column = 0)
+	public function fetchField(int $column = 0): mixed
 	{
 		if (func_num_args()) {
 			trigger_error(__METHOD__ . '() argument is deprecated.', E_USER_DEPRECATED);
@@ -245,7 +244,7 @@ class ResultSet implements \Iterator, IRowContainer
 	 * @param  string|int  $key  column name used for an array key or null for numeric index
 	 * @param  string|int  $value  column name used for an array value or null for the whole row
 	 */
-	public function fetchPairs($key = null, $value = null): array
+	public function fetchPairs(string|int $key = null, string|int $value = null): array
 	{
 		return Helpers::toPairs($this->fetchAll(), $key, $value);
 	}
@@ -266,7 +265,6 @@ class ResultSet implements \Iterator, IRowContainer
 
 	/**
 	 * Fetches all rows and returns associative tree.
-	 * @param  string  $path  associative descriptor
 	 */
 	public function fetchAssoc(string $path): array
 	{

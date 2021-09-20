fc23725b2d71649083280b749533a326afdcc9f2
David Grudl <david@grudl.com>
2021-09-21 01:06:36 +0200

    added Nette\Database\QueryException

---
+++ src/Database/QueryException.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of the Nette Framework (https://nette.org)
+ * Copyright (c) 2004 David Grudl (https://davidgrudl.com)
+ */
+
+declare(strict_types=1);
+
+namespace Nette\Database;
+
+
+class QueryException extends DriverException
+{
+	public string $queryString;
+	private array $params;
+
+
+	/** @internal */
+	public function setQueryInfo(string $queryString, array $params): void
+	{
+		$this->queryString = $queryString;
+		$this->params = $params;
+	}
+
+
+	public function getQueryString(): string
+	{
+		return $this->queryString;
+	}
+
+
+	public function getParameters(): array
+	{
+		return $this->params;
+	}
+}

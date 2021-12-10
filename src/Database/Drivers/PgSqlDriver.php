0122ddccbbce3986fc458b60fde8e9ad07e4cde2
Miloslav Hůla <miloslav.hula@gmail.com>
2021-12-10 11:51:39 +0100

    PgSqlDriver: getColumns() for materialized view too

--- src/Database/Drivers/PgSqlDriver.php
+++ src/Database/Drivers/PgSqlDriver.php
@@ -131,7 +131,7 @@ class PgSqlDriver extends PdoDriver
 				LEFT JOIN pg_catalog.pg_attrdef AS ad ON ad.adrelid = c.oid AND ad.adnum = a.attnum
 				LEFT JOIN pg_catalog.pg_constraint AS co ON co.connamespace = c.relnamespace AND contype = 'p' AND co.conrelid = c.oid AND a.attnum = ANY(co.conkey)
 			WHERE
-				c.relkind IN ('r', 'v')
+				c.relkind IN ('r', 'v', 'm')
 				AND c.oid = {$this->pdo->quote($this->delimiteFQN($table))}::regclass
 				AND a.attnum > 0
 				AND NOT a.attisdropped

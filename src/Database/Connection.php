49c05dbb2cf67d3660f0e4e2981e2d7d92060768
David Grudl <david@grudl.com>
2022-01-19 14:05:47 +0100

    deprecated methods trigger notices

--- src/Database/Connection.php
+++ src/Database/Connection.php
@@ -110,6 +110,7 @@ class Connection
 	/** @deprecated use getDriver() */
 	public function getSupplementalDriver(): Driver
 	{
+		trigger_error(__METHOD__ . '() is deprecated, use getDriver()', E_USER_DEPRECATED);
 		$this->connect();
 		return $this->driver;
 	}
@@ -228,6 +229,7 @@ class Connection
 	/** @deprecated  use query() */
 	public function queryArgs(string $sql, array $params): ResultSet
 	{
+		trigger_error(__METHOD__ . '() is deprecated, use query()', E_USER_DEPRECATED);
 		return $this->query($sql, ...$params);
 	}
 

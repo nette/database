990eb8bf4d4828929f611b5948c265b5a552ff6f
David Grudl <david@grudl.com>
2023-12-13 18:33:50 +0100

    some others methods moved to PdoDriver

--- src/Database/Drivers/PdoDriver.php
+++ src/Database/Drivers/PdoDriver.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace Nette\Database\Drivers;
 
 use Nette;
+use Nette\Database\DriverException;
 use PDO;
 use PDOException;
 
@@ -42,5 +43,80 @@ abstract class PdoDriver implements Nette\Database\Driver
 	public function getPdo(): ?PDO
 	{
 		return $this->pdo;
+	}
+
+
+	public function query(string $queryString, array $params)
+	{
+		try {
+			$types = ['boolean' => PDO::PARAM_BOOL, 'integer' => PDO::PARAM_INT, 'resource' => PDO::PARAM_LOB, 'NULL' => PDO::PARAM_NULL];
+
+			$statement = $this->pdo->prepare($queryString);
+			foreach ($params as $key => $value) {
+				$type = gettype($value);
+				$statement->bindValue(is_int($key) ? $key + 1 : $key, $value, $types[$type] ?? PDO::PARAM_STR);
+			}
+
+			$statement->setFetchMode(PDO::FETCH_ASSOC);
+			@$statement->execute(); // @ PHP generates warning when ATTR_ERRMODE = ERRMODE_EXCEPTION bug #73878
+			return $statement;
+
+		} catch (PDOException $e) {
+			$e = $this->convertException($e);
+			$e->queryString = $queryString;
+			$e->params = $params;
+			throw $e;
+		}
+	}
+
+
+	public function beginTransaction(): void
+	{
+		try {
+			$this->pdo->beginTransaction();
+		} catch (PDOException $e) {
+			throw $this->convertException($e);
+		}
+	}
+
+
+	public function commit(): void
+	{
+		try {
+			$this->pdo->commit();
+		} catch (PDOException $e) {
+			throw $this->convertException($e);
+		}
+	}
+
+
+	public function rollBack(): void
+	{
+		try {
+			$this->pdo->rollBack();
+		} catch (PDOException $e) {
+			throw $this->convertException($e);
+		}
+	}
+
+
+	public function getInsertId(?string $sequence = null): string
+	{
+		try {
+			$res = $this->pdo->lastInsertId($sequence);
+			return $res === false ? '0' : $res;
+		} catch (PDOException $e) {
+			throw $this->convertException($e);
+		}
+	}
+
+
+	public function quote(string $string, int $type = PDO::PARAM_STR): string
+	{
+		try {
+			return $this->pdo->quote($string, $type);
+		} catch (PDOException $e) {
+			throw DriverException::from($e);
+		}
 	}
 }

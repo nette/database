0aed4609420e1bf4b33861766b7c729737953363
David Grudl <david@grudl.com>
2024-08-16 04:44:00 +0200

    added new drivers

---
+++ src/Database/Drivers/Driver.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the Nette Framework (https://nette.org)
+ * Copyright (c) 2004 David Grudl (https://davidgrudl.com)
+ */
+
+declare(strict_types=1);
+
+namespace Nette\Database\Drivers;
+
+
+/**
+ * Creates connection and database engine instances.
+ */
+interface Driver
+{
+	/** Creates a engine instance for the specific database platform. */
+	function createEngine(): Engine;
+}
---
+++ src/Database/Drivers/PDO/Driver.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the Nette Framework (https://nette.org)
+ * Copyright (c) 2004 David Grudl (https://davidgrudl.com)
+ */
+
+declare(strict_types=1);
+
+namespace Nette\Database\Drivers\PDO;
+
+use Nette\Database\Drivers;
+
+
+/**
+ * Base PDO database driver.
+ */
+abstract class Driver implements Drivers\Driver
+{
+}

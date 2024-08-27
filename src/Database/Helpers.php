66430fe25c9be4eafcb59f42415d7b5c529cef72
David Grudl <david@grudl.com>
2024-08-27 19:44:43 +0200

    returns date-time as immutable Nette\Database\DateTime by default (BC break)
    can be disabled by option newDateTime = false

--- src/Database/Helpers.php
+++ src/Database/Helpers.php
@@ -207,7 +207,7 @@ class Helpers
 	public static function normalizeRow(
 		array $row,
 		ResultSet $resultSet,
-		$dateTimeClass = Nette\Utils\DateTime::class,
+		$dateTimeClass = DateTime::class,
 	): array
 	{
 		foreach ($resultSet->getColumnTypes() as $key => $type) {

c9fd5a90c2193cd6753e6cf892f3924814b4a334
David Grudl <david@grudl.com>
2024-09-03 23:09:29 +0200

    Engine::applyLimit() returns string (BC break)

--- src/Database/Table/SqlBuilder.php
+++ src/Database/Table/SqlBuilder.php
@@ -87,7 +87,7 @@ class SqlBuilder
 		}
 
 		if ($this->limit !== null || $this->offset) {
-			$this->engine->applyLimit($query, $this->limit, $this->offset);
+			$query = $this->engine->applyLimit($query, $this->limit, $this->offset);
 		}
 
 		return $query;
@@ -98,7 +98,7 @@ class SqlBuilder
 	{
 		$query = "DELETE FROM {$this->delimitedTable}" . $this->tryDelimite($this->buildConditions());
 		if ($this->limit !== null || $this->offset) {
-			$this->engine->applyLimit($query, $this->limit, $this->offset);
+			$query = $this->engine->applyLimit($query, $this->limit, $this->offset);
 		}
 
 		return $query;
@@ -185,7 +185,7 @@ class SqlBuilder
 		$queryJoins = $this->buildQueryJoins($joins, $finalJoinConditions);
 		$query = "{$querySelect} FROM {$this->delimitedTable}{$queryJoins}{$queryCondition}{$queryEnd}";
 
-		$this->engine->applyLimit($query, $this->limit, $this->offset);
+		$query = $this->engine->applyLimit($query, $this->limit, $this->offset);
 
 		return $this->tryDelimite($query);
 	}

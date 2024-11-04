73ca21b4dae54d62e32f73adf043a4162e4eae7f
David Grudl <david@grudl.com>
2024-11-04 13:49:27 +0100

    DatabaseExtension: added 'username' as alias for 'user'

--- src/Bridges/DatabaseDI/DatabaseExtension.php
+++ src/Bridges/DatabaseDI/DatabaseExtension.php
@@ -31,6 +31,7 @@ class DatabaseExtension extends Nette\DI\CompilerExtension
 			Expect::structure([
 				'dsn' => Expect::string()->required()->dynamic(),
 				'user' => Expect::string()->nullable()->dynamic(),
+				'username' => Expect::string()->nullable()->dynamic(),
 				'password' => Expect::string()->nullable()->dynamic(),
 				'options' => Expect::array(),
 				'debugger' => Expect::bool(),
@@ -96,7 +97,7 @@ class DatabaseExtension extends Nette\DI\CompilerExtension
 		$cache = new Statement(Nette\Caching\Cache::class, [1 => $cacheId]);
 
 		$explorer = $builder->addDefinition($this->prefix($name))
-			->setFactory(Nette\Database\Explorer::class, [$config->dsn, $config->user, $config->password, $config->options])
+			->setFactory(Nette\Database\Explorer::class, [$config->dsn, $config->username ?? $config->user, $config->password, $config->options])
 			->addSetup('setCache', [$cache])
 			->setAutowired($config->autowired);
 

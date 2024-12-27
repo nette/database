0c304d48b62f4f04bf147531d9d1d28655f204e3
David Grudl <david@grudl.com>
2024-12-27 08:46:22 +0100

    Reflection: added 'scale' field

--- src/Database/Driver.php
+++ src/Database/Driver.php
@@ -65,7 +65,7 @@ interface Driver
 
 	/**
 	 * Returns metadata for all columns in a table.
-	 * @return list<array{name: string, table: string, nativeType: string, size: ?int, nullable: bool, default: mixed, autoIncrement: bool, primary: bool, comment: ?string, vendor: array}>
+	 * @return list<array{name: string, table: string, nativeType: string, size: ?int, scale: ?int, nullable: bool, default: mixed, autoIncrement: bool, primary: bool, comment: ?string, vendor: array}>
 	 */
 	function getColumns(string $table): array;
 

5b387369e4f85a88837dcb9b47084fe873ba7e46
David Grudl <david@grudl.com>
2025-01-11 03:16:25 +0100

    Explorer, Selection, Structure: change of constructor dependencies (BC break)

--- src/Database/Explorer.php
+++ src/Database/Explorer.php
@@ -26,7 +26,7 @@ class Explorer
 		private readonly Connection $connection,
 		private readonly IStructure $structure,
 		?Conventions $conventions = null,
-		private readonly ?Nette\Caching\Storage $cacheStorage = null,
+		private readonly ?Nette\Caching\Cache $cache = null,
 	) {
 		$this->conventions = $conventions ?: new StaticConventions;
 	}
@@ -86,7 +86,7 @@ class Explorer
 	 */
 	public function table(string $table): Table\Selection
 	{
-		return new Table\Selection($this, $this->conventions, $table, $this->cacheStorage);
+		return new Table\Selection($this, $table);
 	}
 
 
@@ -127,7 +127,13 @@ class Explorer
 		string $column,
 	): Table\GroupedSelection
 	{
-		return new Table\GroupedSelection($this, $this->conventions, $table, $column, $refSelection, $this->cacheStorage);
+		return new Table\GroupedSelection($this, $table, $column, $refSelection);
+	}
+
+
+	public function getCache(): ?Nette\Caching\Cache
+	{
+		return $this->cache;
 	}
 
 

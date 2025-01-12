5d5498a9c0d5d4216c3235db4613979be361c0fa
David Grudl <david@grudl.com>
2025-01-12 15:34:41 +0100

    Connection::getDsn() deprecated (BC break)

--- src/Database/Connection.php
+++ src/Database/Connection.php
@@ -45,7 +45,7 @@ class Connection
 
 
 	public function __construct(
-		private readonly string $dsn,
+		string $dsn,
 		?string $username = null,
 		#[\SensitiveParameter]
 		?string $password = null,
@@ -108,9 +108,10 @@ class Connection
 	}
 
 
+	/** @deprecated */
 	public function getDsn(): string
 	{
-		return $this->dsn;
+		throw new Nette\DeprecatedException(__METHOD__ . '() is deprecated.');
 	}
 
 

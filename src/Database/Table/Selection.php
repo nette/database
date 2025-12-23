b4be7deac060c799915212889b0d20ecc707c40b
David Grudl <david@grudl.com>
2025-12-23 22:46:53 +0100

    used PHP 8.2 features

--- src/Database/Table/Selection.php
+++ src/Database/Table/Selection.php
@@ -800,10 +800,7 @@ class Selection implements \Iterator, IRowContainer, \ArrayAccess, \Countable
 			$return = $this->explorer->query($this->sqlBuilder->buildInsertQuery() . ' ' . $data->getSql(), ...$data->getSqlBuilder()->getParameters());
 
 		} else {
-			if ($data instanceof \Traversable) {
-				$data = iterator_to_array($data);
-			}
-
+			$data = iterator_to_array($data);
 			$return = $this->explorer->query($this->sqlBuilder->buildInsertQuery() . ' ?values', $data);
 		}
 
@@ -873,10 +870,7 @@ class Selection implements \Iterator, IRowContainer, \ArrayAccess, \Countable
 	 */
 	public function update(iterable $data): int
 	{
-		if ($data instanceof \Traversable) {
-			$data = iterator_to_array($data);
-		}
-
+		$data = iterator_to_array($data);
 		if (!$data) {
 			return 0;
 		}

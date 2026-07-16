3549b8446b5fa4ab09b5211d933d1d04841ecca0
David Grudl <david@grudl.com>
2026-07-16 14:44:02 +0200

    MsSqlDriver: reflection no longer fatals on table names without an explicit schema, defaults to dbo

--- src/Database/Drivers/MsSqlDriver.php
+++ src/Database/Drivers/MsSqlDriver.php
@@ -140,7 +140,7 @@ class MsSqlDriver implements Nette\Database\Driver
 
 	public function getColumns(string $table): array
 	{
-		[$table_schema, $table_name] = explode('.', $table);
+		[$table_schema, $table_name] = $this->splitTableName($table);
 		$columns = [];
 
 		$rows = $this->connection->query(<<<'X'
@@ -186,7 +186,7 @@ class MsSqlDriver implements Nette\Database\Driver
 
 	public function getIndexes(string $table): array
 	{
-		[, $table_name] = explode('.', $table);
+		[, $table_name] = $this->splitTableName($table);
 		$indexes = [];
 
 		$rows = $this->connection->query(<<<'X'
@@ -223,7 +223,7 @@ class MsSqlDriver implements Nette\Database\Driver
 
 	public function getForeignKeys(string $table): array
 	{
-		[$table_schema, $table_name] = explode('.', $table);
+		[$table_schema, $table_name] = $this->splitTableName($table);
 		$keys = [];
 
 		$rows = $this->connection->query(<<<'X'
@@ -266,5 +266,14 @@ class MsSqlDriver implements Nette\Database\Driver
 	public function getColumnTypes(\PDOStatement $statement): array
 	{
 		return Nette\Database\Helpers::detectTypes($statement);
+	}
+
+
+	/** @return array{string, string} schema and table name */
+	private function splitTableName(string $table): array
+	{
+		return str_contains($table, '.')
+			? explode('.', $table, 2)
+			: ['dbo', $table];
 	}
 }
